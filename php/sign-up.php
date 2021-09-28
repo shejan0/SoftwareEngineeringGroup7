a8f62a745b645d0f63d55d4905b9b7e85b4cd495
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2021-09-28 03:15:19 -0500

    updated php for signup and login, connected homepage, signup, and login to database

--- php/sign-up.php
+++ php/sign-up.php
@@ -1,6 +1,11 @@
 <?php
+$name = filter_input(INPUT_POST, 'name');
 $username = filter_input(INPUT_POST, 'username');
-$password = filter_input(INPUT_POST, 'password');
+$password = filter_input(INPUT_POST, 'user_password');
+$email = filter_input(INPUT_POST, 'user_email');
+$usertype = filter_input(INPUT_POST, 'usertype');
+$hoteltype = filter_input(INPUT_POST, 'hoteltype');
+
 if (!empty($username)) {
     if (!empty($password)) {
         $host = "swe-project-db.ckec3iue5fvo.us-east-2.rds.amazonaws.com";
@@ -10,10 +15,13 @@ if (!empty($username)) {
 
         // Create connection
         $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+        // insert into tables
         if (mysqli_connect_error())
             die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
         else {
-            $sql = "INSERT INTO login (username, password) values ('$username','$password')";
+            $sql = "INSERT INTO 
+            signup (name,email,password,username,user_type,hotel_type) values ('$name','$email','$password','$username','$usertype','$hoteltype')";
 
             if ($conn->query($sql))
                 echo "New record is inserted sucessfully";
