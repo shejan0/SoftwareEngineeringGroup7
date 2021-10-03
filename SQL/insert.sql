23114e5027e9e184c321a3db81c2e9e05e6b01e0
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2021-10-03 16:37:03 -0500

    created and inserted some hotel values

--- SQL/insert.sql
+++ SQL/insert.sql
@@ -1,8 +1,22 @@
-insert into admin(name,email,password)
-values
-("Kevin Nguyen", "kevin0@gmail.com","admin"),
-("Shejan Shuza", "shejan1@gmail.com","admin"),
-("Viswa Bhargavi", "viswa2@gmail.com","admin"),
-("Philip Lam", "philip3@gmail.com","admin"),
-("Jonathan Garcia", "jonathan4@gmail.com","admin")
-;
+INSERT INTO admin
+    (name,email,password)
+VALUES
+    ("Kevin Nguyen", "kevin0@gmail.com", "admin"),
+    ("Shejan Shuza", "shejan1@gmail.com", "admin"),
+    ("Viswa Bhargavi", "viswa2@gmail.com", "admin"),
+    ("Philip Lam", "philip3@gmail.com", "admin"),
+    ("Jonathan Garcia", "jonathan4@gmail.com", "admin");
+
+INSERT INTO hotel
+    (hotelName, number_of_rooms)
+VALUES
+    ("The Magnolia All Suites",20),
+    ("The Lofts at Town Centre",60),
+    ("Park North Hotel",100),
+    ("The Courtyard Suites",20),
+    ("The Regency Rooms",20),
+    ("Town Inn Budget Rooms",150),
+    ("The Comfy Motel Place",50),
+    ("Sun Palace Inn",50),
+    ("HomeAway Inn",30),
+    ("Rio Inn",30);
