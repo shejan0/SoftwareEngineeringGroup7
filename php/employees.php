368a5097d49ca1e92e251f57ac9b87b33b45e08f
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2021-10-03 11:53:45 -0500

    created dashboard include file

--- php/employees.php
+++ php/employees.php
@@ -1,10 +1,10 @@
 <?php
 include_once "inc/connection.php";
 include_once "inc/header.php";
-include_once "inc/dashboard-header-nav.php";
+include_once "inc/dashboard/dashboard-header-nav.php";
 ?>
 <main class="content">
-   <?php include_once "inc/dashboard-main-nav.php";?>
+   <?php include_once "inc/dashboard/dashboard-main-nav.php";?>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
         <div class="d-block mb-4 mb-md-0">
             <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
@@ -93,7 +93,7 @@ include_once "inc/dashboard-header-nav.php";
                     <?php  } ?>
         </table>
     </div>
-    <?php include_once "inc/dashboard-footer.php"?>
+    <?php include_once "inc/dashboard/dashboard-footer.php"?>
 <!-- Core -->
 <script src="../vendor/@popperjs/core/dist/umd/popper.min.js"></script>
 <script src="../vendor/bootstrap/dist/js/bootstrap.min.js"></script>
