f1710025f4e658e2e1c7befcf46b65ec967fecfc
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2021-10-06 11:03:25 -0500

    added conection

--- php/inc/user-connection.php
+++ php/inc/user-connection.php
@@ -1,7 +1,12 @@
 <?php
-            $conn = mysqli_connect("swe-project-db.ckec3iue5fvo.us-east-2.rds.amazonaws.com", "admin", "softwareengineering", "user");
+// $db_host = $_ENV['DB_HOST'];
+// $db_username = $_ENV['DB_USERNAME'];
+// $db_password = $_ENV['DB_PASSWORD'];
+// $db_name = $_ENV['DB_DATABASE'];
 
-            if (mysqli_connect_errno()) {
-                echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                exit();
-            }
+$conn = mysqli_connect("swe-project-db.ckec3iue5fvo.us-east-2.rds.amazonaws.com", "admin", "softwareengineering", "user");
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
