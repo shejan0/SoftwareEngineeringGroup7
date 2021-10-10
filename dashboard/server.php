9748ffba880a74a0271f7186a993809c40f96efe
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2021-10-10 01:40:30 -0500

    added an alert if employee email is taken

--- dashboard/server.php
+++ dashboard/server.php
@@ -8,6 +8,7 @@ $password = $_POST['password'];
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 $email = $_POST['email'];
 $username = $_POST['username'];
+$errors = array();
 
 // sign up 
 if (isset($_POST['sign-up'])) {
@@ -48,19 +49,23 @@ if (isset($_POST['sign-up'])) {
         }
     }
 }
+// adding admin / employee
 if (isset($_POST['admin-sign-up'])) {
     if (!empty($email)) {
         if (!empty($password)) {
             $check_email = "SELECT * FROM admin WHERE email=?";
-    
+
             $stmt_email = $conn->prepare($check_email);
             $stmt_email->bind_param('s', $email);
             $stmt_email->execute();
             $stmt_email->store_result();
 
             // if email is taken
-            if ($stmt_email->num_rows() > 0 ) {
-                header("location: ../html/sign-up-error.html");
+            if ($stmt_email->num_rows() > 0) {
+                echo "<script>
+                window.location.href='employees.php';
+                alert('Email already taken');
+                </script>";
             }
             // if  email isn't taken, insert into database
             else {
@@ -70,7 +75,12 @@ if (isset($_POST['admin-sign-up'])) {
                     $stmt->bind_param('sss', $name, $email, $password);
                     $stmt->execute();
                     $stmt->store_result();
-                    header("location: employees.php");
+                    
+                    echo "<script>
+                    alert('Employee Successfully added!');
+                    window.location.href='employees.php';
+                    </script>";
+
                     die();
                 }
             }
@@ -124,7 +134,7 @@ if (isset($_POST['admin-sign-in'])) {
 
         // preparing the SQL statement
         if ($stmt = $conn->prepare($sql)) {
-            $stmt->bind_param('sss', $_POST['email'],$_POST['password'],$_POST['name']);
+            $stmt->bind_param('sss', $_POST['email'], $_POST['password'], $_POST['name']);
             $stmt->execute();
             $stmt->store_result(); // Store the result so we can check if the account exists in the database.
 
