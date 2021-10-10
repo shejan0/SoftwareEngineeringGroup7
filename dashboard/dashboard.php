3cdb2ffcb6c625a088cc5fcd1424cae4d551f01d
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2021-10-10 16:09:18 -0500

    updated dashboard vie

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -12,56 +12,6 @@ if(!isset($_SESSION['email']))
   <?php include_once "inc/header.php";?>
   
     <div class="py-4">
-      <div class="dropdown">
-        <button class="btn btn-gray-800 d-inline-flex align-items-center me-2 dropdown-toggle" data-bs-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-          <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-          </svg>
-          New Task
-        </button>
-        <div class="dropdown-menu dashboard-dropdown dropdown-menu-start mt-2 py-1">
-          <a class="dropdown-item d-flex align-items-center" href="#">
-            <svg class="dropdown-icon text-gray-400 me-2" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z">
-              </path>
-            </svg>
-            Add Widget
-          </a>
-          <a class="dropdown-item d-flex align-items-center" href="#">
-            <svg class="dropdown-icon text-gray-400 me-2" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M5.5 13a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 13H11V9.413l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13H5.5z">
-              </path>
-              <path d="M9 13h2v5a1 1 0 11-2 0v-5z"></path>
-            </svg>
-            Upload Files
-          </a>
-          <a class="dropdown-item d-flex align-items-center" href="#">
-            <svg class="dropdown-icon text-gray-400 me-2" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"></path>
-            </svg>
-            Preview Security
-          </a>
-          <div role="separator" class="dropdown-divider my-1"></div>
-          <a class="dropdown-item d-flex align-items-center" href="#">
-            <svg class="dropdown-icon text-danger me-2" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
-                clip-rule="evenodd"></path>
-            </svg>
-            Upgrade to Pro
-          </a>
-        </div>
-      </div>
     </div>
     <div class="row">
       <div class="col-12 mb-4">
@@ -221,150 +171,12 @@ if(!isset($_SESSION['email']))
     <div class="row">
       <div class="col-12 col-xl-8">
         <div class="row">
-          <div class="col-12 mb-4">
-            <div class="card border-0 shadow">
-              <div class="card-header">
-                <div class="row align-items-center">
-                  <div class="col">
-                    <h2 class="fs-5 fw-bold mb-0">Page visits</h2>
-                  </div>
-                  <div class="col text-end">
-                    <a href="#" class="btn btn-sm btn-primary">See all</a>
-                  </div>
-                </div>
-              </div>
-              <div class="table-responsive">
-                <table class="table align-items-center table-flush">
-                  <thead class="thead-light">
-                    <tr>
-                      <th class="border-bottom" scope="col">Page name</th>
-                      <th class="border-bottom" scope="col">Page Views</th>
-                      <th class="border-bottom" scope="col">Page Value</th>
-                      <th class="border-bottom" scope="col">Percentage</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th class="text-gray-900" scope="row">
-                        /demo/admin/index.html
-                      </th>
-                      <td class="fw-bolder text-gray-500">
-                        3,225
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        $20
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        <div class="d-flex">
-                          <svg class="icon icon-xs text-danger me-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          42.55%
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th class="text-gray-900" scope="row">
-                        /demo/admin/forms.html
-                      </th>
-                      <td class="fw-bolder text-gray-500">
-                        2,987
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        0
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        <div class="d-flex">
-                          <svg class="icon icon-xs text-success me-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          43.24%
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th class="text-gray-900" scope="row">
-                        /demo/admin/util.html
-                      </th>
-                      <td class="fw-bolder text-gray-500">
-                        2,844
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        294
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        <div class="d-flex">
-                          <svg class="icon icon-xs text-success me-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          32.35%
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th class="text-gray-900" scope="row">
-                        /demo/admin/validation.html
-                      </th>
-                      <td class="fw-bolder text-gray-500">
-                        2,050
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        $147
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        <div class="d-flex">
-                          <svg class="icon icon-xs text-danger me-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          50.87%
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th class="text-gray-900" scope="row">
-                        /demo/admin/modals.html
-                      </th>
-                      <td class="fw-bolder text-gray-500">
-                        1,483
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        $19
-                      </td>
-                      <td class="fw-bolder text-gray-500">
-                        <div class="d-flex">
-                          <svg class="icon icon-xs text-success me-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          26.12%
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
           <div class="col-12 col-xxl-6 mb-4">
             <div class="card border-0 shadow">
               <div class="card-header border-bottom d-flex align-items-center justify-content-between">
                 <h2 class="fs-5 fw-bold mb-0">Admin / Manager
                 </h2>
-                <a href="#" class="btn btn-sm btn-primary">See all</a>
+                <a href="employees.php" class="btn btn-sm btn-primary">See all</a>
               </div>
               <div class="card-body">
                 <ul class="list-group list-group-flush list my--3">
@@ -373,7 +185,7 @@ if(!isset($_SESSION['email']))
                       <div class="col-auto">
                         <!-- Avatar -->
                         <a href="#" class="avatar">
-                          <img class="rounded" alt="Image placeholder" src="../assets/img/team/avatar.jpeg">
+                          <img class="rounded" alt="Image placeholder" src="../assets/img/team/discord.png">
                         </a>
                       </div>
                       <div class="col-auto ms--2">
@@ -384,17 +196,6 @@ if(!isset($_SESSION['email']))
                           <div class="bg-success dot rounded-circle me-1"></div>
                           <small>Online</small>
                         </div>
-                      </div>
-                      <div class="col text-end">
-                        <a href="#" class="btn btn-sm btn-primary d-inline-flex align-items-center">
-                          <svg class="icon icon-xxs me-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          Invite
-                        </a>
                       </div>
                     </div>
                   </li>
@@ -415,17 +216,6 @@ if(!isset($_SESSION['email']))
                           <small>Busy</small>
                         </div>
                       </div>
-                      <div class="col text-end">
-                        <a href="#" class="btn btn-sm btn-primary d-inline-flex align-items-center">
-                          <svg class="icon icon-xxs me-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          Message
-                        </a>
-                      </div>
                     </div>
                   </li>
                   <li class="list-group-item px-0">
@@ -445,17 +235,6 @@ if(!isset($_SESSION['email']))
                           <small>Offline</small>
                         </div>
                       </div>
-                      <div class="col text-end">
-                        <a href="#" class="btn btn-sm btn-primary d-inline-flex align-items-center">
-                          <svg class="icon icon-xxs me-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          Message
-                        </a>
-                      </div>
                     </div>
                   </li>
                   <li class="list-group-item px-0">
@@ -474,17 +253,6 @@ if(!isset($_SESSION['email']))
                           <div class="bg-danger dot rounded-circle me-1"></div>
                           <small>Offline</small>
                         </div>
-                      </div>
-                      <div class="col text-end">
-                        <a href="#" class="btn btn-sm btn-primary d-inline-flex align-items-center">
-                          <svg class="icon icon-xxs me-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                              d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
-                              clip-rule="evenodd"></path>
-                          </svg>
-                          Message
-                        </a>
                       </div>
                     </div>
                   </li>
@@ -601,33 +369,6 @@ if(!isset($_SESSION['email']))
         </div>
       </div>
       <div class="col-12 col-xl-4">
-        <div class="col-12 px-0 mb-4">
-          <div class="card border-0 shadow">
-            <div class="card-header d-flex flex-row align-items-center flex-0 border-bottom">
-              <div class="d-block">
-                <div class="h6 fw-normal text-gray mb-2">Total bookings</div>
-                <h2 class="h3 fw-extrabold">452</h2>
-                <div class="small mt-2">
-                  <span class="fas fa-angle-up text-success"></span>
-                  <span class="text-success fw-bold">18.2%</span>
-                </div>
-              </div>
-              <div class="d-block ms-auto">
-                <div class="d-flex align-items-center text-end mb-2">
-                  <span class="dot rounded-circle bg-gray-800 me-2"></span>
-                  <span class="fw-normal small">July</span>
-                </div>
-                <div class="d-flex align-items-center text-end">
-                  <span class="dot rounded-circle bg-secondary me-2"></span>
-                  <span class="fw-normal small">August</span>
-                </div>
-              </div>
-            </div>
-            <div class="card-body p-2">
-              <div class="ct-chart-ranking ct-golden-section ct-series-a"></div>
-            </div>
-          </div>
-        </div>
         <div class="col-12 px-0 mb-4">
           <div class="card border-0 shadow">
             <div class="card-body">
