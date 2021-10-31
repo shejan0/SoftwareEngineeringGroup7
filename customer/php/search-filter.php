f5c8d12906a5675c1b5da6d5ba462b1112973a9b
Shejan Shuza <shejan0@gmail.com>
2021-10-30 19:28:59 -0500

    Added code for database pulling and filling of the form, needs logic for form search now.

--- customer/php/search-filter.php
+++ customer/php/search-filter.php
@@ -1,9 +1,13 @@
-<form action="#" autocomplete="off" method="get">
+<?php
+include_once "user-connection.php";
+
+?>
+<form autocomplete="off" method="get"> <!--Get the action back to the host page -->
           <div class="row">
             <div class="col-xl-4 col-md-6 mb-4 ">
               <label class="form-label" for="form_dates">Dates</label>
               <div class="datepicker-container datepicker-container-left">
-                <input class="form-control input-group btn-pill bg-white shadow-soft border-light" type="text" name="bookingDate" id="form_dates"
+                <input class="form-control  input-group btn-pill bg-white shadow-soft border-light" type="text" name="bookingDate" id="form_dates"
                   placeholder="Choose your dates">
               </div>
             </div>
@@ -11,21 +15,33 @@
               <label class="form-label" for="form_guests">Guests</label>
               <select class="form-select input-group btn-pill bg-white  shadow-soft border-light" name="guests" id="form_guests" 
                 title=" ">
-                <option value="1">1 </option>
-                <option value="2">2 </option>
-                <option value="3">3 </option>
-                <option value="3">4 </option>
-                <option value="4">5 </option>
+
+                <?php
+                for($i = 1; $i<=10;$i++){
+                  echo "<option value\"$i\">$i</option>";
+                }
+                ?>
+
               </select>
             </div>
             <div class="col-xl-4 col-md-6 mb-4">
               <label class="form-label" for="form_type">Room type</label>
-              <select class="form-select input-group btn-pill bg-white shadow-soft border-light" name="guests" id="form_guests" 
+              <select class="form-select input-group btn-pill bg-white shadow-soft border-light" name="roomType" id="form_guests" 
               title=" ">
-                <option value="type_0">Any </option>
-                <option value="type_1">Standard suite </option>
-                <option value="type_2">Queen suite</option>
-                <option value="type_3">King suite</option>
+                <?php
+                  $roomTypequery = "SELECT DISTINCT typeName FROM RoomType;";
+                  $result = $conn->query($roomTypequery);
+                  //print_r($result);
+                  if($result->num_rows<=0){
+                    echo "<option>FAILED</option>";
+                  }else{
+                    while($roomType = $result->fetch_assoc()){
+                      
+                      $name=$roomType["typeName"];
+                      echo "<option value=\"$name\">$name</option>";
+                   }
+                  }
+                ?>
               </select>
             </div>
             <div class="col-xl-4 col-md-6 mb-4">
@@ -42,55 +58,39 @@
               <div>
                 <label class="form-label">Amentities</label>
                 <ul class="list-inline mb-0 mt-1">
-                  <li class="list-inline-item">
-                    <div class="form-check form-switch">
-                      <input class="form-check-input " id="" type="checkbox">
-                      <label class="form-check-label" for=""> <span class="text-sm">Wifi</span></label>
-                    </div>
-                  </li>
-                  <li class="list-inline-item">
-                    <div class="form-check form-switch">
-                      <input class="form-check-input " id="superhost" type="checkbox">
-                      <label class="form-check-label" for="superhost"> <span class="text-sm">Some amentities</span></label>
-                    </div>
-                  </li>
-                  <li class="list-inline-item">
-                    <div class="form-check form-switch">
-                      <input class="form-check-input " id="superhost" type="checkbox">
-                      <label class="form-check-label" for="superhost"> <span class="text-sm">Some amentities</span></label>
-                    </div>
-                  </li>
-                  <li class="list-inline-item">
-                    <div class="form-check form-switch">
-                      <input class="form-check-input " id="superhost" type="checkbox">
-                      <label class="form-check-label" for="superhost"> <span class="text-sm">Some amentities</span></label>
-                    </div>
-                  </li>
-                  <li class="list-inline-item">
-                    <div class="form-check form-switch">
-                      <input class="form-check-input " id="superhost" type="checkbox">
-                      <label class="form-check-label" for="superhost"> <span class="text-sm">Some amentities</span></label>
-                    </div>
-                  </li>
-                  <li class="list-inline-item">
-                    <div class="form-check form-switch">
-                      <input class="form-check-input " id="superhost" type="checkbox">
-                      <label class="form-check-label" for="superhost"> <span class="text-sm">Some amentities</span></label>
-                    </div>
-                  </li>
-                  <li class="list-inline-item">
-                    <div class="form-check form-switch">
-                      <input class="form-check-input " id="superhost" type="checkbox">
-                      <label class="form-check-label" for="superhost"> <span class="text-sm">Some amentities</span></label>
-                    </div>
-                  </li>
+                  <?php
+                    $amenityquery = "SELECT DISTINCT amenityName FROM GenAmenities;";
+                    $result = $conn->query($amenityquery);
+                    if($result->num_rows<=0){
+                      echo "<li class=\"list-inline-item\">";
+                      echo "<div class=\"form-check form-switch\">";
+                      echo "<input class=\"form-check-input \" id=\"\" type=\"checkbox\">";
+                      echo "<label class=\"form-check-label\" for=\"\"> <span class=\"text-sm\">FAILED</span></label>";
+                      echo "</div>";
+                      echo "</li>";
+                    }else{
+                      while($amenity = $result->fetch_assoc()){
+                        $aname=$amenity["amenityName"];
+                        echo "<li class=\"list-inline-item\">";
+                        echo "<div class=\"form-check form-switch\">";
+                        if(isset($_GET[$aname])){
+                          echo "<input class=\"form-check-input \" id=\"\" name=\"$aname\" type=\"checkbox\" checked>";  
+                        }else{
+                          echo "<input class=\"form-check-input \" id=\"\" name=\"$aname\" type=\"checkbox\">";
+                        }
+                        echo "<label class=\"form-check-label\" for=\"\"> <span class=\"text-sm\">$aname</span></label>";
+                        echo "</div>";
+                        echo "</li>";
+                      }
+                    }
+                  ?>
                 </ul>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col-sm-6 mb-4 order-2 order-sm-1">
-              <button class="btn btn-primary shadow-soft border-light animate-up-2" type="submit"> <i class="fas fa-search me-1"></i>Search </button>
+              <button class="btn btn-primary shadow-soft border-light animate-up-2" type="submit"> <i class="fas fa-search me-1"></i>Search</button>
             </div>
           </div>
         </form>
