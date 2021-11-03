296a17e394c55d432899f474082de2a21baa7c43
Shejan Shuza <shejan0@gmail.com>
2021-11-03 00:22:24 -0500

    Updated hotel description view for dyanmic

--- customer/room-details.php
+++ customer/room-details.php
@@ -1,7 +1,7 @@
 <?php
 include_once "php/head.php";
 include_once "php/header.php";
-
+include_once "../php/inc/user-connection.php"
 ?>
     <div class="container py-5">
       <div class="row">
@@ -14,15 +14,49 @@ include_once "php/header.php";
                     </nav>
         <div class="col-lg-8"> 
           <div class="text-block">
+            <!--
             <p class="text-primary"><i class="fa-map-marker-alt fa me-1"></i> Brooklyn, New York</p>
-            <h1>Mid-Century Modern Garden Paradise</h1>
+            -->
+            <?php
+            if(empty($_GET["hotelID"])){
+              echo "<h1>FAILED</h1>";
+            }else{
+              $namequery = "SELECT hotelName FROM hotel.hotel WHERE hotelID=?";
+              $prepared = $conn->prepare($namequery);
+              $prepared->bind_param("i",$_GET["hotelID"]);
+              $prepared->execute();
+              $prepared->bind_result($name);
+              $prepared->fetch();
+              echo "<h1>$name</h1>";
+              $prepared->close();
+            }
+            
+            ?>
+            <!--
             <p class="text-muted text-uppercase mb-4">Entire Apartment </p>
+            
             <ul class="list-inline text-sm mb-4">
               <li class="list-inline-item me-3"><i class="fa fa-users me-1 text-secondary"></i> 4 guests</li>
               <li class="list-inline-item me-3"><i class="fa fa-door-open me-1 text-secondary"></i> 1 bedroom</li>
               <li class="list-inline-item me-3"><i class="fa fa-bed me-1 text-secondary"></i> 3 beds</li>
               <li class="list-inline-item me-3"><i class="fa fa-bath me-1 text-secondary"></i> 1 bath</li>
             </ul>
+           -->
+            <?php
+              if(empty($_GET["hotelID"])){
+                echo "<p class=\"text-muted fw-light\">FAILED</p>";
+              }else{
+                $descquery = "SELECT hotelDesc FROM hotel.Descriptions WHERE hotelID=?;";
+                $prepared = $conn->prepare($descquery);
+                $prepared->bind_param("i",$_GET["hotelID"]);
+                $prepared->execute();
+                $prepared->bind_result($desc);
+                $prepared->fetch();
+                echo "<p class=\"text-muted fw-light\">$desc</p>";
+                $prepared->close();
+              }
+            ?>
+            <!--
             <p class="text-muted fw-light">Our garden basement apartment is fully equipped with everything you need to enjoy your stay. Very comfortable for a couple but plenty of space for a small family. Close to many wonderful Brooklyn attractions and quick trip to Manhattan. </p>
             <h6 class="mb-3">The space</h6>
             <p class="text-muted fw-light">Welcome to Brooklyn! We are excited to share our wonderful neighborhood with you. Our modern apartment has a private entrance, fully equipped kitchen, and a very comfortable queen size bed. We are happy to accommodate additional guests with a single bed in the living room, another comfy mattress on the floor and can make arrangements for small children with a portable crib and highchair if requested. </p>
@@ -38,13 +72,38 @@ include_once "php/header.php";
             <p class="text-muted fw-light">The apartment is surprisingly quiet for being in the heart of a vibrant, bustling neighborhood.</p>
             <h6 class="mb-3">Interaction with guests</h6>
             <p class="text-muted fw-light">We live in the two floors above the garden apartment so we are usually available to answer questions. The garden apartment is separate from our living space. We are happy to provide advice on local attractions, restaurants and transportation around the city. If there's anything you need please don't hesitate to ask!</p>
+            -->
           </div>
+          
           <div class="text-block">
             <h4 class="mb-4">Amenities</h4>
             <div class="row"> 
+              <?php
+                if(empty($_GET["hotelID"])){
+                  echo "<div class=\"col-md-6\">";
+                  echo "<ul class=\"text-muted\">";
+                  echo "<li class=\"mb-2\"><span class=\"text-sm\">FAILED</span></li>";
+                  echo "</ul>";
+                  echo "</div>";
+                }else{
+                  $ameniquery="SELECT amenityName FROM hotel.GenAmenities WHERE hotelID=?;";
+                  $prepared=$conn->prepare($ameniquery);
+                  $prepared->bind_param("i",$_GET["hotelID"]);
+                  $prepared->execute();
+                  $prepared->bind_result($amenity);
+                  echo "<div class=\"col-md-6\">";
+                  echo "<ul class=\"text-muted\">";  
+                  while($prepared->fetch()){
+                    echo "<li class=\"mb-2\"><span class=\"text-sm\">$amenity</span></li>";
+                  }
+                  echo "</ul></div>";
+                }
+              ?>
+              <!--
               <div class="col-md-6">
-                <ul class="list-unstyled text-muted">
-                  <li class="mb-2"> <i class="fa fa-wifi text-secondary w-1rem me-3 text-center"></i><span class="text-sm">Wifi</span></li>
+                <ul class="text-muted">
+                  
+                  <li class="mb-2"><span class="text-sm">Wifi</span></li>
                   <li class="mb-2"> <i class="fa fa-tv text-secondary w-1rem me-3 text-center"></i><span class="text-sm">Cable TV</span></li>
                   <li class="mb-2"> <i class="fa fa-snowflake text-secondary w-1rem me-3 text-center"></i><span class="text-sm">Air conditioning</span></li>
                   <li class="mb-2"> <i class="fa fa-thermometer-three-quarters text-secondary w-1rem me-3 text-center"></i><span class="text-sm">Heating</span></li>
@@ -58,12 +117,33 @@ include_once "php/header.php";
                   <li class="mb-2"> <i class="fa fa-tshirt text-secondary w-1rem me-3 text-center"></i><span class="text-sm">Washing machine</span></li>
                 </ul>
               </div>
+              -->
             </div>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="p-4 shadow ms-lg-4 rounded sticky-top" style="top: 100px;">
-            <p class="text-muted"><span class="text-primary h2">$xxx</span> per night</p>
+            <?php
+              if(empty($_GET["hotelID"])){
+               echo "<p class=\"text-muted\"><span class=\"text-primary h2\">FAILED</span></p>";
+              }else{
+                $pricequery="SELECT priceStandard,priceQueen,priceKing FROM hotel.hotel WHERE hotelID=?;";
+                $prepared=$conn->prepare($pricequery);
+                $prepared->bind_param("i",$_GET["hotelID"]);
+                $prepared->execute();
+                $prepared->bind_result($priceStandard,$priceQueen,$priceKing);
+                $prepared->fetch();
+                if($priceStandard!=0){
+                  echo "<p class=\"text-muted\"><span class=\"text-primary h2\">$$priceStandard</span> per night <b>STANDARD</b></p>";
+                }
+                if($priceQueen!=0){
+                  echo "<p class=\"text-muted\"><span class=\"text-primary h2\">$$priceQueen</span> per night <b>QUEEN</b></p>";
+                }
+                if($priceKing!=0){
+                  echo "<p class=\"text-muted\"><span class=\"text-primary h2\">$$priceKing</span> per night <b>KING</b></p>";
+                }
+              }
+            ?>
             <hr class="my-4">
             <form class="form" id="booking-form" method="get" action="#" autocomplete="off">
               <div class="mb-4">
@@ -74,13 +154,13 @@ include_once "php/header.php";
               </div>
               </div>
               <div class="mb-4">
-                <label class="form-label" for="guests">Guests *</label>
-                <select class="form-control input-group btn-pill bg-white shadow-soft border-light" name="guests" id="guests">
-                  <option value="1">1 Guest</option>
-                  <option value="2">2 Guests</option>
-                  <option value="3">3 Guests</option>
-                  <option value="4">4 Guests</option>
-                  <option value="5">5 Guests</option>
+                <label class="form-label" for="rooms">Rooms *</label>
+                <select class="form-control input-group btn-pill bg-white shadow-soft border-light" name="rooms" id="rooms">
+                  <?php
+                    for($i = 1; $i<=10;$i++){
+                      echo "<option value\"$i\">$i</option>";
+                    }
+                  ?>
                 </select>
               </div>
               <div class="d-grid mb-4">
