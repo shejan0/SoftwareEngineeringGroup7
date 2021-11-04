b54d5a0196c2bb43acd8753a7873a138e12cb5cd
LostSounds404 <71282692+LostSounds404@users.noreply.github.com>
2021-11-04 16:37:58 -0500

    Update footer.php
    Fixed link from logo to redirect to root

--- customer/php/footer.php
+++ customer/php/footer.php
@@ -39,7 +39,7 @@
           <div class="col-lg-2 col-md-6 mb-5 mb-lg-0">
             <h6 class="text-uppercase text-dark mb-3">Rentals</h6>
             <ul class="list-unstyled">
-              <li><a class="text-muted" href="../index.html">Home</a></li>
+              <li><a class="text-muted" href="../">Home</a></li>
             </ul>
           </div>
           <div class="col-lg-4">
