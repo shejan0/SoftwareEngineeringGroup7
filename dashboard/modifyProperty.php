049dc07632d7fef9d22057122d139f3bf53eb7a6
Viswa Bhargavi <abishekvishwa.v@gmail.com>
2021-11-09 23:30:05 -0600

    fixed modify

--- dashboard/modifyProperty.php
+++ dashboard/modifyProperty.php
@@ -16,7 +16,7 @@ mysqli_close($conn);
 
 ?>
 <html lang="en-US">
-    <form>
+    <form action="modifyProperty.php" method="post">
         <div><br><label for="hotelID">Enter Hotel ID of Hotel to be Modified (required):</label><input type="text" name="hotelID"><br></div>
         <br><input type="submit" name="enter" value="Enter"><br>
     </form>
