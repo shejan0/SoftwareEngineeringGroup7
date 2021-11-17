b985d4aa597e3971f409a9dff362c3b568432313
Philip Lam <philip-a-lam@hotmail.com>
2021-11-16 23:27:14 -0600

    Beginning reservation window

---
+++ customer/reserve.php
@@ -0,0 +1,10 @@
+<?php
+    function reserve($hotelID, $roomID, $email, $dates, $normPrice, $surgePrice, $numRes )
+    {
+        my( $arrival, $departure ) = ($dates =~ m/(\d+-\d+-\d+) to (\d+-\d+-\d+)/)
+        $numDays = 
+        $weekDays = 
+        $weekendDays = 
+        $totalPrice = 
+    }
+?>
