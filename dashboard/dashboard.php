921867fc170d20bdfad4c7c938ed544a5860d44b
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2021-11-18 03:41:57 -0600

    .

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -10,12 +10,25 @@ if(!isset($_SESSION['email']))
 ?>
   <main class="content bg-white">
   <?php include_once "inc/header.php";?>
-  <li class="breadcrumb-item active" aria-current="page">
-        <a href=" <?php echo basename(__FILE__) ?>"><?php echo basename(__FILE__, '.php') ?>
-        </a>
-    </li>
-    </ol>
-    </nav>
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+        <div class="d-block mb-4 mb-md-0">
+            <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+                <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+                    <li class="breadcrumb-item">
+                        <a href="dashboard.php">
+                            <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                                </path>
+                            </svg>
+                        </a>
+                    </li>  <li class="breadcrumb-item active" aria-current="page">
+    <a href=" <?php echo basename(__FILE__) ?>"><?php echo basename(__FILE__, '.php') ?>
+    </a>
+  </li>
+  </ol>
+  </nav>
+  </div>
+  </div>
     <div class="row">
       <div class="col-12 mb-4">
         <div class="card bg-white shadow-soft border-light text-center py-4 animate-up-5">
