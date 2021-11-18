8b5dd2eee8a39174a497cb2304ca3e2882ba5441
Viswa Bhargavi <abishekvishwa.v@gmail.com>
2021-11-17 21:33:22 -0600

    logic done but...

--- dashboard/modifyProperty.php
+++ dashboard/modifyProperty.php
@@ -1,4 +1,10 @@
-<?php include_once "inc/user-connection.php";?>
+<?php 
+include_once "../php/inc/user-connection.php";
+include_once "validateProperty.php";
+
+$header = "modifyProperty.php";
+$all_amenities = array();  
+?>
 <html lang="en-US">
     <head><title>Modify Property</title></head>
     <h1>Modify Property</h1>
@@ -23,27 +29,29 @@
                     echo "<h3>Current Hotel Info for Hotel ID " . $hotelID . "</h3>";
                     echo "Hotel Name: " . $hotelProp['hotelName'] . "<br>";
                     echo "Total number of rooms: " . $hotelProp['number_of_rooms'] . "<br>";
-                    echo "Number of Standard Rooms: " . $hotelProp['numStandard'] . "<br>";
-                    echo "Number of Queen Rooms: " . $hotelProp['numQueen'] . "<br>";
                     echo "Number of King Rooms: " . $hotelProp['numKing'] . "<br>";
-                    echo "Price of Standard Rooms: " . $hotelProp['priceStandard'] . "<br>";
-                    echo "Price of Queen Rooms: " . $hotelProp['priceQueen'] . "<br>";
+                    echo "Number of Queen Rooms: " . $hotelProp['numQueen'] . "<br>";
+                    echo "Number of Standard Rooms: " . $hotelProp['numStandard'] . "<br>";
                     echo "Price of King Rooms: " . $hotelProp['priceKing'] . "<br>";
+                    echo "Price of Queen Rooms: " . $hotelProp['priceQueen'] . "<br>";
+                    echo "Price of Standard Rooms: " . $hotelProp['priceStandard'] . "<br>";
                     echo "Weekend Surcharge: " . $hotelProp['weekendSurge'] . "<br>"; 
                     $amenityQuery = "SELECT * FROM `hotel`.`GenAmenities` WHERE hotelID = $hotelID";
                     $amenityResult = mysqli_query($conn, $amenityQuery); 
                     $amenityRows = mysqli_num_rows($amenityResult);
-                    echo "Currently amenities for \"" . $hotelProp['hotelName'] . "\":";
+                    echo "Currently amenities for \"" . $hotelProp['hotelName'] . "\": ";
                     if ($amenityRows == 0) echo "N/A<br>";
-                    else while ($amenity = mysqli_fetch_assoc($amenityResult)) {
-                        $all_amenities[] = $amenity['amenityName'];
-                        echo " | " . $amenity['amenityName'] . " | ";
+                    else {
+                        while ($amenity = mysqli_fetch_assoc($amenityResult)) {
+                            $all_amenities[] = $amenity['amenityName'];
+                            echo $amenity['amenityName'] . " ";
+                        }
                     }
                     ?>
-                    <h3>Fill fields to be Modified. Empty fields will not be modified</h3> <strong>Hotel ID:<?php echo $hotelID ?></strong>
+                    <h3>Fill fields that need Modification.</h3> <strong>Hotel ID: <?php echo $hotelID ?></strong>
                     <form action="modifyProperty.php" method="post">
-                        <div><br><label for="hotelName">Enter Hotel Name:</label><input type="text" name="hotelName"><br></div>
-                        <div><br><label for="numRooms">Enter Total number of Rooms (required):</label> <input type="text" name="numRooms"><br></div>
+                        <div><br><label for="hotelName">Enter Hotel Name (optional):</label><input type="text" name="hotelName"><br></div>
+                        <div><br><label for="totalRooms">Enter Total number of Rooms (optional):</label> <input type="text" name="totalRooms"><br></div>
                         <div>
                             <br><label>Select Amenities (optional):
                             <br>(Note: All current amenitites are checked. To modify check aditional or uncheck current)</label><br>
@@ -67,43 +75,74 @@
                             <?php if ($hotelProp['numStandard']>0) echo 'checked="checked"'; ?>><br>
                         </div>
                         <div>
-                            <br><label>Enter Price for each Room Type included:</label><br>
-                            <label for="priceKing">Price for King</label> <input type="text" name="priceKing"><br>
-                            <label for="priceQueen">Price for Queen</label> <input type="text" name="priceQueen"><br>
-                            <label for="priceStandard">Price for Standard</label> <input type="text" name="priceStandard"><br>
+                            <br><label>Enter Price for each Room Type selected:
+                            <br>(Note: Current prices auto-filled, modify based on selected room types.)</label><br>
+                            <label for="priceKing">Price for King</label> <input type="text" name="priceKing" value=<?php echo $hotelProp['priceKing']; ?>><br>
+                            <label for="priceQueen">Price for Queen</label> <input type="text" name="priceQueen" value=<?php echo $hotelProp['priceQueen']; ?>><br>
+                            <label for="priceStandard">Price for Standard</label> <input type="text" name="priceStandard" value=<?php echo $hotelProp['priceStandard']; ?>><br>
                         </div>
-                        <div><br><label for="weekendSurge">Enter Weekend Surcharge(Required):</label> <input type="text" name="weekendSurge"><br></div>
+                        <div><br><label for="weekendSurge">Enter Weekend Surcharge(optional):</label> <input type="text" name="weekendSurge"><br></div>
                         <br><input type="submit" name="modify" value="Modify Property"><br>
                     </form>               
                     <?php
                     if (isset($_POST["modify"])) {    // all process provided below at each break point
-                        //update hotel name
-                        if (!empty($_POST['hotelName'])) {
-                            if ($_POST['hotelName'] != $hotelProp['hotelName']) {
-                                $hotelName = $_POST['hotelName'];
-                                $updateNameQuery = "UPDATE `hotel`.`hotel` SET hotelName = '$hotelName' WHERE hotelID='$hotelID'";
-                                $updateNameResult = mysqli_query($conn, $updateNameQuery);
-                                if (!$updateNameResult) exit("<p class='error'>Error Updating Hotel Name: ($updateNameQuery) " . mysqli_error($conn) . "</p>");
-                                echo "<p>Successfully Updated Hotel Name \"" . $hotelName . "\"</p>";
+                        //initialize vars
+                        $hotelName = $_POST['hotelName'];
+                        $totalRooms = $_POST['totalRooms'];
+                        $king=$_POST['king'];
+                        $queen=$_POST['queen'];
+                        $standard=$_POST['standard'];
+                        $priceKing = $_POST['priceKing'];
+                        $priceQueen = $_POST['priceQueen'];   
+                        $priceStandard = $_POST['priceStandard'];
+                        $pool = $_POST['pool'];
+                        $gym = $_POST['gym'];
+                        $spa = $_POST['spa'];
+                        $businessOffice = $_POST['businessOffice'];
+                        $weekendSurge=$_POST['weekendSurge'];
+
+                        if (!empty($hotelName)) {
+                            if ($hotelName != $hotelProp['hotelName']) {
+                                $updateQuery = "UPDATE hotel.hotel SET hotelName = '$hotelName' WHERE hotelID = '$hotelID'";
+                                $updateResult = mysqli_query($conn, $updateQuery);
+                                if (!$updateResult) exit("<p class='error'>Error Updating Hotel Name: ($updateQuery) " . mysqli_error($conn) . "</p>");
+                                echo "<p>Successfully Updated Hotel Name to \"" . $hotelName . "\"</p>";
                             }
-                            else echo "Entered current Hotel name. Value unchaged<br>";
                         }
-                        else echo "Hotel Name Field blank. Value unchaged<br>";
                         
                         //update total rooms
-                        if (!empty($_POST['numRooms'])) {
-                            if (!ctype_digit($_POST['numRooms'])) echo "Error: Enter \"positive integer\" for Total Number of Rooms<br>";
-                            if ($_POST['numRooms'] != $hotelProp['number_of_rooms']) {
-                                $numRooms = $_POST['numRooms'];
-                                $updateNumRoomsQuery = "UPDATE `hotel`.`hotel` SET number_of_rooms = '$numRooms' WHERE hotelID='$hotelID'";
-                                $updateNumRoomsResult = mysqli_query($conn, $updateNumRoomsQuery);
-                                if (!$updateNumRoomsResult) exit("<p class='error'>Error Updating Total Number of rooms: ($updateNumRoomsQuery) " . mysqli_error($conn) . "</p>");
-                                echo "<p>Successfully Updated Total Number of rooms \"" . $hotelName . "\"</p>";
+                        if (!empty($totalRooms)) {
+                            validateTotalRooms($totalRooms, $header);
+                            if ($totalRooms != $hotelProp['number_of_rooms']) {
+                                $updateQuery = "UPDATE `hotel`.`hotel` SET number_of_rooms='$totalRooms' WHERE hotelID = '$hotelID'";
+                                $updateResult = mysqli_query($conn, $updateQuery);
+                                if (!$updateResult) exit("<p class='error'>Error Updating Total Number of rooms: ($updateQuery) " . mysqli_error($conn) . "</p>");
+                                echo "<p>Successfully Updated Total Number of rooms to \"" . $totalRooms . "\"</p>";
                             }
-                            else echo "Entered current Total number of rooms. Value unchaged<br>";
                         }
-                        else echo "Total Rooms Field Blank. Value Unchaged<br>";
 
+
+                        [$numKing, $numQueen, $numStandard] = calcNumRooms($king, $queen, $standard, $totalRooms, $header);
+                        validatePrice($king, $queen, $standard, $priceKing, $priceQueen, $priceStandard, $header);
+
+                        $updateQuery = "UPDATE `hotel`.`hotel` SET numKing='$numKing', numQueen='$numQueen', numStandard='$numStandard', 
+                        priceKing='$priceKing', priceQueen='$priceQueen' priceStandard='$priceStandard' WHERE hotelID='$hotelID'";
+                        $updateResult = mysqli_query($conn, $updateQuery);
+
+                        if (!empty($weekendSurge)) {
+                            validateWeekendSurge($weekendSurge, $header);
+                            if ($weekendSurge != $hotelProp['weekendSurge']) {
+                                $updateQuery = "UPDATE `hotel`.`hotel` SET weekendSurge='$weekendSurge' WHERE hotelID = '$hotelID'";
+                                $updateResult = mysqli_query($conn, $updateQuery);
+                                if (!$updateResult) exit("<p class='error'>Error Updating Total Number of rooms: ($updateQuery) " . mysqli_error($conn) . "</p>");
+                                echo "<p>Successfully Updated Weekend Surge to \"" . $weekendSurge . "\"</p>";
+                            }
+                        }
+                        // delete amenity to then re-add based on updated amenities
+                        $deleteAmenitiesQuery = "DELETE FROM `hotel`.`GenAmenities` WHERE hotelID = '$hotelID'";
+                        $deleteAmenitiesResult = mysqli_query($conn, $deleteAmenitiesQuery);
+                        // check and insert amenities to GenAmenities table
+                        validateAmenities($pool, $gym, $spa, $businessOffice, $hotelID, $header, $conn);
                     }
                 }
             }
