52c3cf5fc16da61840ecb96ebaa2b2b671a87960
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2021-11-18 04:13:14 -0600

    fixed breadcrumbs

--- dashboard/employees.php
+++ dashboard/employees.php
@@ -5,90 +5,98 @@ include_once "inc/side-bar.php";
 ?>
 <main class="content bg-white">
     <?php include_once "inc/header.php"; ?>
-    <li class="breadcrumb-item active" aria-current="page">
-        <a href=" <?php echo basename(__FILE__) ?>"><?php echo basename(__FILE__, '.php') ?>
-        </a>
-    </li>
-    </ol>
-    </nav>
-    <h2 class="h4">Employees</h2>
-    <p class="mb-0">List of employees and admin information.</p>
-    </div>
-    <div class="btn mb-2 mb-md-0">
-        <button type="button" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center animate-up-2" data-bs-toggle="modal" data-bs-target="#modalSignUp">
-            <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-            </svg>
-            Add employee
-        </button>
-        <button type="button" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center animate-up-2" data-bs-toggle="modal" data-bs-target="#modalSignUp">
-            <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-            </svg>
-            Add employee
-        </button>
-        <!-- Modal Content -->
-        <div class="modal fade" id="modalSignUp" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body px-md-5">
-                        <h2 class="h4 text-center">Employee information</h2>
-                        <form action="server.php" method=POST>
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+            <div class="d-block mb-4 mb-md-0">
+                <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+                    <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+                        <li class="breadcrumb-item">
+                            <a href="dashboard.php">
+                                <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                                    </path>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            <a href=" <?php echo basename(__FILE__) ?>"><?php echo basename(__FILE__, '.php') ?>
+                            </a>
+                        </li>
+                    </ol>
+                </nav>
+                <h2 class="h4">Employees</h2>
+                <p class="mb-0">Employee tables</p>
+            </div>
+       
+        <div class="btn mb-2 mb-md-0">
+            <button type="button" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center animate-up-2" data-bs-toggle="modal" data-bs-target="#modalSignUp">
+                <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                </svg>
+                Add employee
+            </button>
+            <!-- Modal Content -->
+            <div class="modal fade" id="modalSignUp" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body px-md-5">
+                            <h2 class="h4 text-center">Employee information</h2>
+                            <form action="../php/server.php" method=POST>
 
-                            <!-- Modal Form -->
+                                <!-- Modal Form -->
 
-                            <!-- Name -->
-                            <div class="form-group mb-4">
-                                <label for="name">Name</label>
-                                <div class="input-group">
-                                    <span class="input-group-text border-gray-300" id="basic-addon5">
-                                        <span class="fas fa-user"></span>
-                                    </span>
-                                    <input type="text" placeholder="Name" class="form-control border-gray-300" id="name" name="name" required>
-                                </div>
-                            </div>
-
-                            <!-- email -->
-                            <div class="form-group mb-4">
-                                <label for="email">Email</label>
-                                <div class="input-group">
-                                    <span class="input-group-text border-gray-300" id="basic-addon3">
-                                        <svg class="icon icon-xxs" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                                        </svg>
-                                    </span>
-                                    <input type="email" class="form-control border-gray-300" placeholder="example@company.com" id="email" name='email' autofocus required>
-                                </div>
-                            </div>
-
-                            <!-- password -->
-                            <div class="form-group">
+                                <!-- Name -->
                                 <div class="form-group mb-4">
-                                    <label for="password">Password</label>
+                                    <label for="name">Name</label>
                                     <div class="input-group">
-                                        <span class="input-group-text border-gray-300" id="basic-addon4">
-                                            <svg class="icon icon-xxs"" fill=" currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
+                                        <span class="input-group-text border-gray-300" id="basic-addon5">
+                                            <span class="fas fa-user"></span>
+                                        </span>
+                                        <input type="text" placeholder="Name" class="form-control border-gray-300" id="name" name="name" required>
+                                    </div>
+                                </div>
+
+                                <!-- email -->
+                                <div class="form-group mb-4">
+                                    <label for="email">Email</label>
+                                    <div class="input-group">
+                                        <span class="input-group-text border-gray-300" id="basic-addon3">
+                                            <svg class="icon icon-xxs" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                                             </svg>
                                         </span>
-                                        <input type="password" placeholder="Password" class="form-control border-gray-300" id="password" name='password' required>
+                                        <input type="email" class="form-control border-gray-300" placeholder="example@company.com" id="email" name='email' autofocus required>
                                     </div>
-</div>
-                            </div>
-                            <!-- submit -->
-                            <div class="d-grid">
-                                <button type="submit" class="btn btn-primary" name='admin-sign-up'>Submit</button>
-                            </div>
-                        </form>
+                                </div>
+
+                                <!-- password -->
+                                <div class="form-group">
+                                    <div class="form-group mb-4">
+                                        <label for="password">Password</label>
+                                        <div class="input-group">
+                                            <span class="input-group-text border-gray-300" id="basic-addon4">
+                                                <svg class="icon icon-xxs" fill=" currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path>
+                                                </svg>
+                                            </span>
+                                            <input type="password" placeholder="Password" class="form-control border-gray-300" id="password" name='password' required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- submit -->
+                                <div class="d-grid">
+                                    <button type="submit" class="btn btn-primary" name='admin-sign-up'>Submit</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>     
-    </div>
+        </div>
+</div>
     </div>
     <div class="table-settings mb-4">
         <div class="row align-items-center justify-content-between">
--- dashboard/hotel.php
+++ dashboard/hotel.php
@@ -5,20 +5,33 @@ include_once "inc/side-bar.php";
 ?>
 <main class="content bg-white">
     <?php include_once "inc/header.php"; ?>
-    <li class="breadcrumb-item active" aria-current="page">
-        <a href=" <?php echo basename(__FILE__) ?>"><?php echo basename(__FILE__, '.php') ?>
-        </a>
-    </li>                </ol>
-            </nav>
-            <h2 class="h4">Hotel</h2>
-            <p class="mb-0">Search hotel.</p>
-        </div>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+            <div class="d-block mb-4 mb-md-0">
+                <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+                    <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+                        <li class="breadcrumb-item">
+                            <a href="dashboard.php">
+                                <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                                    </path>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            <a href=" <?php echo basename(__FILE__) ?>"><?php echo basename(__FILE__, '.php') ?>
+                            </a>
+                        </li>
+                    </ol>
+                </nav>
+                <h2 class="h4">Hotel</h2>
+                <p class="mb-0">Hotel table - add or edit hotels.</p>
+            </div>
+       
         <div class="btn mb-2 mb-md-0">
-        <a href="/property/createProperty.php" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center animate-up-2">Create Property</a>
-        <a href="/property/modifyProperty.php" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center animate-up-2">Modify Property</a>
-
+            <a href="../property/createProperty.php" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center animate-up-2">Add Property </a>
+            <a href="../property/modifyProperty.php"class="btn btn-sm btn-gray-800 d-inline-flex align-items-center animate-up-2">ModifyProperty </a>
+        </div>
 </div>
-    </div>
     <div class="table-settings mb-4">
         <div class="row align-items-center justify-content-between">
             <div class="col col-md-6 col-lg-3 col-xl-4">
