405cf04812670843cb23a2d91c9d50efe2ee5409
Kevin Nguyen <Kevin.Nguyen6079@gmail.com>
2021-11-20 07:03:11 -0600

    added logic to add reservation (still need code to insert into reservation table)

--- customer/book.php
+++ customer/book.php
@@ -5,31 +5,72 @@ include_once "../php/inc/user-connection.php";
 
 $id = $_GET['hotelID'];
 $date = $_GET['bookingDate'];
+$dateRange = explode(" to", $_GET['bookingDate']);
+$start = $dateRange[0];
+$end = $dateRange[1];
 $numRooms = $_GET['rooms'];
 $roomType = $_GET['type'];
 $email = $_SESSION['email'];
 $name = $_SESSION['name'];
 
 $query = "SELECT * from hotel where hotelID = $id";
-$result = mysqli_query($conn,$query);
+$result = mysqli_query($conn, $query);
 $records = mysqli_fetch_assoc($result);
 
-if(isset($_GET['book'])){
-    if ($roomType == 'queen' && !empty($date) && !empty($numRooms)){
-    $update = "UPDATE hotel set numQueen = $records[numQueen] - $numRooms where hotelID = $id";
-    $updateResult = mysqli_query($conn,$update);
+if (isset($_GET['book'])) {
+    if ($roomType == 'standard' && !empty($date)) {
 
-    $_SESSION['alert'] = "alert alert-success alert-dismissible fade show";
-    $_SESSION['message'] = "Successfully booked your reservation";
-    header("location: room-details.php?".$_SERVER['QUERY_STRING']);
-    exit();
-    }
-    else{
+        // if room avaliable
+        if ($numRooms <= $records['numStandard']) {
+            $update = "UPDATE hotel set numStandard = $records[numStandard] - $numRooms where hotelID = $id;";
+            $updateResult = mysqli_query($conn, $update);
+
+            $_SESSION['alert'] = "alert alert-success alert-dismissible fade show";
+            $_SESSION['message'] = "Successfully booked your reservation from " . $start .  " to " . $end;
+            header("location: room-details.php?" . $_SERVER['QUERY_STRING']);
+            exit();
+        } else {
+            $_SESSION['alert'] = "alert alert-danger alert-dismissible fade show";
+            $_SESSION['message'] = "Error: The Room you are trying to book is full";
+            header("location: room-details.php?" . $_SERVER['QUERY_STRING']);
+            exit();
+        }
+    } else if ($roomType == 'queen' && !empty($date)) {
+         // if room avaliable
+        if ($numRooms <= $records['numQueen']) {
+            $update = "UPDATE hotel set numQueen = $records[numQueen] - $numRooms where hotelID = $id;";
+            $updateResult = mysqli_query($conn, $update);
+            
+            $_SESSION['alert'] = "alert alert-success alert-dismissible fade show";
+            $_SESSION['message'] = "Successfully booked your reservation from " . $start .  " to " . $end;
+            header("location: room-details.php?" . $_SERVER['QUERY_STRING']);
+            exit();
+        }  else {
+            $_SESSION['alert'] = "alert alert-danger alert-dismissible fade show";
+            $_SESSION['message'] = "Error: The Room you are trying to book is full";
+            header("location: room-details.php?" . $_SERVER['QUERY_STRING']);
+            exit();
+        }
+    } else if ($roomType == 'king' && !empty($date)) {
+        if ($numRooms <= $records['numKing']) {
+            $update = "UPDATE hotel set numKing = $records[numKing] - $numRooms where hotelID = $id;";
+            $updateResult = mysqli_query($conn, $update);
+
+            $_SESSION['alert'] = "alert alert-success alert-dismissible fade show";
+            $_SESSION['message'] = "Successfully booked your reservation from " . $start .  " to " . $end;
+            header("location: room-details.php?" . $_SERVER['QUERY_STRING']);
+            exit();
+        }
+        else {
+            $_SESSION['alert'] = "alert alert-danger alert-dismissible fade show";
+            $_SESSION['message'] = "Error: The Room you are trying to book is full";
+            header("location: room-details.php?" . $_SERVER['QUERY_STRING']);
+            exit();
+        }
+    } else {
         $_SESSION['alert'] = "alert alert-danger alert-dismissible fade show";
-        $_SESSION['message'] = "Error - Must enter all fields in order to book reservations";
-        header("location: room-details.php?".$_SERVER['QUERY_STRING']);
+        $_SESSION['message'] = "Error: Enter valid dates.";
+        header("location: room-details.php?" . $_SERVER['QUERY_STRING']);
         exit();
     }
 }
-// header("location: room-details.php?".$_SERVER['QUERY_STRING']);
-// exit();
