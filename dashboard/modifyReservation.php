9b40dc80bd618846116953959ce17b1f43dd7df1
Philip Lam <philip-a-lam@hotmail.com>
2021-11-21 23:16:49 -0600

    Creating reservation modify function

--- dashboard/modifyReservation.php
+++ dashboard/modifyReservation.php
@@ -2,5 +2,10 @@
     include_once "inc/session_start.php";
     include_once "../php/inc/user-connection.php";
     include_once "../customer/resConflictCheck.php";
-    
+
+
+    function reservationModify($reservationID, $newRoomType, $newArrival, $newDeparture)
+    {
+        
+    }
 ?>
